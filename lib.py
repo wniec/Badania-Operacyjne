40db62d083d093dc479866eec2f19672866cd80a
naumiec <92885584+naumiec@users.noreply.github.com>
2024-04-22 10:48:27 +0200

    small changes

--- lib.py
+++ lib.py
@@ -1,17 +1,20 @@
-import scipy
-import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse import coo_matrix
 import sys
 import os
+import numpy as np
+import scipy
+from scipy.sparse import coo_matrix, csr_matrix
 import matplotlib.pyplot as plt
+
 np.set_printoptions(threshold=sys.maxsize)
 
-def genSolution(T,M,TEST):
-    print(T,M,TEST)
+
+def genSolution(T, M, TEST):
+    print(f"T={T}, M={M}, TEST={TEST}")
     mach = np.random.choice(list(range(M)), size=TEST, replace=False)
-    tasks = np.hstack((np.random.RandomState().permutation(T), np.random.choice(list(range(T)), size=TEST-T, replace=True)))
-    return mach,tasks
+    tasks = np.hstack((np.random.RandomState().permutation(T),
+                       np.random.choice(list(range(T)), size=TEST-T, replace=True)))
+    return mach, tasks
+
 
 def check_X(X, E, D, eta):
     assert np.all(np.sum(X, axis=0) <= 1)
@@ -30,9 +33,10 @@ def getData(NT, NM):
     print(D.shape)
     print(eta.shape)
     return X, E, D, eta
-        
+
+
 if __name__ == "__main__":
-    NT= 10
+    NT = 10
     NM = 50
 
     E = np.random.random(NT)
@@ -40,7 +44,7 @@ if __name__ == "__main__":
     eta = np.random.random((NT, NM))
     rows, cols = genSolution(NT, NM, NM)
     X = coo_matrix((np.ones(NM), (cols, rows)), shape=(NT, NM)).todense()
-    print(X.shape)
-    print(E.shape)
-    print(D.shape)
-    print(eta.shape)
+    print(f"X shape   = {X.shape}")
+    print(f"E shape   = {E.shape}")
+    print(f"D shape   = {D.shape}")
+    print(f"eta shape = {eta.shape}")
